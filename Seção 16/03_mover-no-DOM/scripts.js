6ba1931aaba09980e93564ef845bc959bb91c858
Gabriel Cerbaro <gabrielis2one@yahoo.com>
2026-06-09 20:35:19 -0300

    Add HTML and JavaScript files for DOM manipulation exercises

---
+++ Seção 16/03_mover-no-DOM/scripts.js
@@ -0,0 +1,13 @@
+console.log(document);
+
+console.log(document.body);
+
+console.log(document.body.childNodes);
+
+console.log(document.body.childNodes[1].childNodes);
+
+console.log(document.body.childNodes[1].childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes[1].textContent);
+
+console.log(document.body.childNodes[1].childNodes[1].innerText);
---
+++ Seção 16/07_query-selector/scripts.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector('.container'));
+
+console.log(document.querySelector('div div h1'));
+
+console.log(document.querySelector('footer h2'));
+
+console.log(document.querySelector('ul .itens-azuis'));
+
+console.log(document.querySelector('.itens-azuis'));
