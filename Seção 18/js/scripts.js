d294bf8df04afc5fa7c712f39b9639ddb6a1ac2c
Gabriel Cerbaro <gabrielis2one@yahoo.com>
2026-06-27 12:15:35 -0300

    Add HTML, CSS, and JavaScript files for Tic Tac Toe game implementation

---
+++ Seção 18/js/scripts.js
@@ -0,0 +1,177 @@
+let x = document.querySelector('.x');
+let o = document.querySelector('.o');
+let boxes = document.querySelectorAll('.box');
+let buttons = document.querySelectorAll('#buttons-container button');
+let messageContainer = document.querySelector('#message');
+let messageText = document.querySelector('#message p');
+let secondPlayer;
+
+// contador de jogadas
+
+let player1 = 0;
+let player2 = 0;
+
+// Adicionando o evento de click aos boxes
+
+for(let i = 0; i < boxes.length; i++) {
+
+    boxes[i].addEventListener('click', function() {
+
+        let el = checkEl(player1, player2);
+
+        // verifica se já tem x ou o
+        if(this.childNodes.length == 0) {
+            let cloneEl = el.cloneNode(true);
+
+            this.appendChild(cloneEl);
+
+            // computar jogada
+
+            if(player1 == player2) {
+            player1++;
+            } else {
+            player2++;
+            }
+
+            // checa qyuem venceu
+            checkWinCondition();
+
+        }
+    });
+}
+
+// ver quem vai jogar
+function checkEl(player1, player2) {
+    if(player1 == player2) {
+        el = x;
+    } else {
+        el = o;
+    }
+
+    return el;
+}
+
+// ve quem ganhou
+function checkWinCondition() {
+
+    let b1 = document.getElementById('block-1')
+    let b2 = document.getElementById('block-2')
+    let b3 = document.getElementById('block-3')
+    let b4 = document.getElementById('block-4')
+    let b5 = document.getElementById('block-5')
+    let b6 = document.getElementById('block-6')
+    let b7 = document.getElementById('block-7')
+    let b8 = document.getElementById('block-8')
+    let b9 = document.getElementById('block-9')
+
+    //horizontal
+    if(b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0) {
+        let b1child = b1.childNodes[0].className;
+        let b2child = b2.childNodes[0].className;
+        let b3child = b3.childNodes[0].className;
+
+        if(b1child == 'x' && b2child == 'x' && b3child == 'x') {
+            console.log('x venceu')
+        } else if (b1child == 'o' && b2child == 'o' && b3child == 'o') {
+            console.log('o venceu')
+        }
+    }
+
+    if(b4.childNodes.length > 0 && b5.childNodes.length > 0 && b6.childNodes.length > 0) {
+        let b4child = b4.childNodes[0].className;
+        let b5child = b5.childNodes[0].className;
+        let b6child = b6.childNodes[0].className;
+
+        if(b4child == 'x' && b5child == 'x' && b6child == 'x') {
+            console.log('x venceu')
+        } else if (b4child == 'o' && b5child == 'o' && b6child == 'o') {
+            console.log('o venceu')
+        }
+    }
+
+    if(b7.childNodes.length > 0 && b8.childNodes.length > 0 && b9.childNodes.length > 0) {
+        let b7child = b7.childNodes[0].className;
+        let b8child = b8.childNodes[0].className;
+        let b9child = b9.childNodes[0].className;
+
+        if(b7child == 'x' && b8child == 'x' && b9child == 'x') {
+            console.log('x venceu')
+        } else if (b7child == 'o' && b8child == 'o' && b9child == 'o') {
+            console.log('o venceu')
+        }
+    }
+
+        if(b1.childNodes.length > 0 && b4.childNodes.length > 0 && b7.childNodes.length > 0) {
+        let b1child = b1.childNodes[0].className;
+        let b4child = b4.childNodes[0].className;
+        let b7child = b7.childNodes[0].className;
+
+        if(b1child == 'x' && b4child == 'x' && b7child == 'x') {
+            console.log('x venceu')
+        } else if (b1child == 'o' && b4child == 'o' && b7child == 'o') {
+            console.log('o venceu')
+        }
+    }
+
+        if(b2.childNodes.length > 0 && b5.childNodes.length > 0 && b8.childNodes.length > 0) {
+        let b2child = b2.childNodes[0].className;
+        let b5child = b5.childNodes[0].className;
+        let b8child = b8.childNodes[0].className;
+
+        if(b2child == 'x' && b5child == 'x' && b8child == 'x') {
+            console.log('x venceu')
+        } else if (b2child == 'o' && b5child == 'o' && b8child == 'o') {
+            console.log('o venceu')
+        }
+    }
+
+        if(b3.childNodes.length > 0 && b6.childNodes.length > 0 && b9.childNodes.length > 0) {
+        let b3child = b3.childNodes[0].className;
+        let b6child = b6.childNodes[0].className;
+        let b9child = b9.childNodes[0].className;
+
+        if(b3child == 'x' && b6child == 'x' && b9child == 'x') {
+            console.log('x venceu')
+        } else if (b3child == 'o' && b6child == 'o' && b9child == 'o') {
+            console.log('o venceu')
+        }
+    }
+
+        if(b1.childNodes.length > 0 && b5.childNodes.length > 0 && b9.childNodes.length > 0) {
+        let b1child = b1.childNodes[0].className;
+        let b5child = b5.childNodes[0].className;
+        let b9child = b9.childNodes[0].className;
+
+        if(b1child == 'x' && b5child == 'x' && b9child == 'x') {
+            console.log('x venceu')
+        } else if (b1child == 'o' && b5child == 'o' && b9child == 'o') {
+            console.log('o venceu')
+        }
+    }
+
+        if(b3.childNodes.length > 0 && b5.childNodes.length > 0 && b7.childNodes.length > 0) {
+        let b3child = b3.childNodes[0].className;
+        let b5child = b5.childNodes[0].className;
+        let b7child = b7.childNodes[0].className;
+
+        if(b3child == 'x' && b5child == 'x' && b7child == 'x') {
+            console.log('x venceu')
+        } else if (b3child == 'o' && b5child == 'o' && b7child == 'o') {
+            console.log('o venceu')
+        }
+    }
+
+    // deu velha
+
+    let counter = 0;
+
+    for(let i = 0; i <boxes.length; i++) {
+        if(boxes[i].childNodes[0] != undefined) {
+            counter++;
+        }
+    }
+
+    if(counter == 9) {
+        console.log('deu velha')
+    }
+}
